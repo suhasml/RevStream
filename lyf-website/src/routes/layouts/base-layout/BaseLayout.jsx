b2352eeec2929892a68dc96797db156d256e34ff
Yashas S <yashas.s2001@gmail.com>
2024-10-26 00:10:22 +0530

    Login Navbar and qr gen added

--- lyf-website/src/routes/layouts/base-layout/BaseLayout.jsx
+++ lyf-website/src/routes/layouts/base-layout/BaseLayout.jsx
@@ -1,5 +1,5 @@
 import GlobalFooter from 'components/global-footer/GlobalFooter';
-import GlobalNavbar from 'components/global-navbar/GlobalNavbar';
+// import GlobalNavbar from 'components/global-navbar/GlobalNavbar';
 import { Outlet } from 'react-router-dom';
 import ScrollToTop from 'components/scroll-to-top/ScrollToTop';
 import Chat from 'routes/chat-bot/Chat';
